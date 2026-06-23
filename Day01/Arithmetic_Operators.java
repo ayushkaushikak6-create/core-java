915490d292e22aed3e45341d1557a2f4a1bb07ad
compilecrew <ayushkaushikak6@gmail.com>
2026-06-23 19:33:54 +0530

    Day 1 completed
    Added variables, data types and arithmetic operator programs.

---
+++ Day01/Arithmetic_Operators.java
@@ -0,0 +1,20 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+
+
+//Write a program that stores: int a = 20 int b = 10 And prints: Sum = 30 Difference = 10 Product = 200 Quotient = 2
+
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotent = a / b;
+
+        System.out.println("sum is " + sum);
+        System.out.println("difference is " + difference);
+        System.out.println("product is " + product);
+        System.out.println("quotent is " + quotent);
+    }
+}
---
+++ Day01/variables_data_types.java
@@ -0,0 +1,28 @@
+
+public class variables_data_types {
+    public static void main(String[] args) {
+        //Create variables for your name, age, and percentage. Print them.
+        String name = "Ayush";
+        int age = 19;
+        double percentage = 78.6;
+
+
+        System.out.println(name );
+        System.out.println(age);
+        System.out.println(percentage);
+
+
+        // Store the price of a laptop in a double variable and print it.
+        double price = 67000;
+
+        System.out.println("Price of laptop is " + price +" INR" );
+
+
+
+        //Create a variable named grade and store your grade in it.
+
+        char grade = 'A';
+
+        System.out.println("Your Grade is " + grade );
+    }
+}
